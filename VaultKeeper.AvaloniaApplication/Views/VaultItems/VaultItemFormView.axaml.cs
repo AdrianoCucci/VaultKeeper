71ae3b31fcd032f3a073249a9d762b1ad840b9aa
Adriano Cucci <cucciadriano@gmail.com>
2026-01-17 01:08:45 -0500

    Add a few finishing touches.

--- VaultKeeper.AvaloniaApplication/Views/VaultItems/VaultItemFormView.axaml.cs
+++ VaultKeeper.AvaloniaApplication/Views/VaultItems/VaultItemFormView.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using System;
 using VaultKeeper.AvaloniaApplication.Extensions;
@@ -16,23 +17,46 @@ public partial class VaultItemFormView : VaultItemViewBase<VaultItemFormViewMode
         InputName.FocusEnd();
     }
 
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+
+        switch (e.Key)
+        {
+            case Key.Enter:
+                Submit();
+                break;
+            case Key.Escape:
+                Cancel();
+                break;
+        }
+    }
+
     private void RaiseEvent(VaultItemFormAction action)
     {
         if (Model == null) return;
         RaiseEvent(action, Model);
     }
 
+    public bool Submit()
+    {
+        bool isValid = Model?.Form?.Validate() == true;
+
+        if (isValid)
+            RaiseEvent(VaultItemFormAction.Submit);
+
+        return isValid;
+    }
+
+    public void Cancel() => RaiseEvent(VaultItemFormAction.Cancel);
+
     private void ActionToggleRevealValue_Click(object? sender, RoutedEventArgs e) => RaiseEvent(VaultItemFormAction.ToggleRevealValue);
 
     private void GenerateValueButton_Click(object? sender, RoutedEventArgs e) => RaiseEvent(VaultItemFormAction.GenerateValue);
 
-    private void FormButtons_Cancelled(object? sender, RoutedEventArgs e) => RaiseEvent(VaultItemFormAction.Cancel);
+    private void FormButtons_Cancelled(object? sender, RoutedEventArgs e) => Cancel();
 
-    private void FormButtons_Submitted(object? sender, RoutedEventArgs e)
-    {
-        Model?.Form?.Validate();
-        RaiseEvent(VaultItemFormAction.Submit);
-    }
+    private void FormButtons_Submitted(object? sender, RoutedEventArgs e) => Submit();
 
     private void Root_LayoutUpdated(object? sender, EventArgs e) => UpdateModel(x => x.UseVerticalLayout = Root.Bounds.Width < 800);
 }
