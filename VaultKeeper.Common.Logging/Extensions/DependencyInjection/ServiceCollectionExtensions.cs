b772424da442e29d3d1420d74fee51e2e770ccc2
Adriano Cucci <cucciadriano@gmail.com>
2026-01-17 20:17:59 -0500

    Implement file/debug logging. (#20)

---
+++ VaultKeeper.Common.Logging/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace VaultKeeper.Common.Logging.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureLogging(this IServiceCollection services, LoggingConfig config) => services.AddLogging(builder =>
+    {
+        const string outputTemplate = "{Timestamp:HH:mm:ss} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}";
+
+        LoggerConfiguration configuration = new LoggerConfiguration()
+            .MinimumLevel.Is((LogEventLevel)config.LogLevel)
+            .Enrich.FromLogContext();
+
+#if DEBUG
+        configuration = configuration.WriteTo.Debug(outputTemplate: outputTemplate);
+#endif
+        if (config.FileLoggingConfig is FileLoggingConfig fileConfig)
+        {
+            configuration.WriteTo.File(
+                fileConfig.FilePath,
+                outputTemplate: outputTemplate,
+                rollingInterval: (RollingInterval)fileConfig.RollingInterval,
+                retainedFileCountLimit: fileConfig.FileLimit);
+        }
+
+        Logger logger = configuration.CreateLogger();
+
+        builder.AddSerilog(logger, true);
+    });
+}
