9a2b9b6684d8ccca0a9831fee70c433416b8295a
Adriano Cucci <cucciadriano@gmail.com>
2026-01-18 13:22:52 -0500

    Implement list virtualization on VaultPage to improve performance, and also add bulk-select when shift-clicking the checkboxes of items.

--- VaultKeeper.AvaloniaApplication/ViewModels/VaultItems/Common/VaultItemActionEventArgs.cs
+++ VaultKeeper.AvaloniaApplication/ViewModels/VaultItems/Common/VaultItemActionEventArgs.cs
@@ -1,17 +1,20 @@
+using Avalonia.Input;
 using Avalonia.Interactivity;
 using System.Diagnostics.CodeAnalysis;
 
 namespace VaultKeeper.AvaloniaApplication.ViewModels.VaultItems.Common;
 
-public class VaultItemActionEventArgs(RoutedEvent routedEvent) : RoutedEventArgs(routedEvent)
+public class VaultItemActionEventArgs(RoutedEvent routedEvent, object? source = null) : RoutedEventArgs(routedEvent, source)
 {
     public required VaultItemViewModelBase ViewModel { get; init; }
     public required VaultItemAction Action { get; init; }
+    public KeyModifiers KeyModifiers { get; init; } = KeyModifiers.None;
 
     [SetsRequiredMembers]
-    public VaultItemActionEventArgs(RoutedEvent routedEvent, VaultItemActionEventArgs other) : this(routedEvent)
+    public VaultItemActionEventArgs(RoutedEvent routedEvent, VaultItemActionEventArgs other, object? source = null) : this(routedEvent, source ?? other.Source)
     {
         ViewModel = other.ViewModel;
         Action = other.Action;
+        KeyModifiers = other.KeyModifiers;
     }
 }
